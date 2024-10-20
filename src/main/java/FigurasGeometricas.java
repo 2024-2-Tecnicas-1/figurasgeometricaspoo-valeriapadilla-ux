fe672bd0a4c673e4cf63cdae013c33103e6c9064
valeriapadilla <paolaperezvaleria@gmail.com>
2024-10-20 13:57:18 -0500

    esta es la original clase de figuras geometricas

---
+++ src/main/java/FigurasGeometricas.java
@@ -0,0 +1,44 @@
+
+public class FigurasGeometricas {
+
+    public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public double obtenerArea() {
+        return 0.0;
+    }
+
+    public double obtenerPerimetro() {
+        return 0.0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+    String obtenerArea() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    String obtenerPerimetro() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }   
+}
